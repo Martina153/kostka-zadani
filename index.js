7096bc7ad8c92893e508feabfc472f0461cf8ec8
Martina_Harsanikova <martinaharsanikova@gmail.com>
2023-03-12 16:38:54 +0100

    updated

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log('funguju!');
+
+//Doplňte do stránky JavaScriptový program, který zařídí, že když uživatel zmáčkne na stránce libovolnou klávesu, 
+
+let side = 1;
+
+window.addEventListener('keydown', function() {
+    const dice = document.querySelector('.dice');
+    side++;
+    if (side === 7) {
+        side = 1;
+    }
+    dice.src = `img/side${side}.svg`;
+});
